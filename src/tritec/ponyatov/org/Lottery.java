e7758e8d188c6e310c0bf7fae399f7fb825fca97
Dmitry Ponyatov <dponyatov@gmail.com>
2015-12-27 12:51:45 +0400

    + lottery
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ src/tritec/ponyatov/org/Lottery.java
@@ -0,0 +1,19 @@
+package tritec.ponyatov.org;
+
+import java.util.Random;
+
+public class Lottery {
+	
+	static int Money = 0x1234;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("unused")
+		Random rnd = new Random(System.currentTimeMillis());
+		while (Money>0) {
+			System.out.println("Your Bid [<number> <bid>]:");
+			
+		}
+	}
+
+}
